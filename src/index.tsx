dff16c6f205e58b27eb8fb43b74444989c6e5f61
Diana Loyuk <wyrrrth.sip@gmail.com>
2025-10-23 09:34:10 +0300

    fix: removed AppProviders; rewrite index from js to ts

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <BrowserRouter>
+    <AuthProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
+  </BrowserRouter>
+);
+
+reportWebVitals();
