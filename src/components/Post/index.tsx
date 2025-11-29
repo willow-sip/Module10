169e786d3e0315cf2c7ffd7e47eef3423c8022c4
Diana Loyuk <wyrrrth.sip@gmail.com>
2025-11-29 21:42:59 +0300

    fix: removed unnecessary loaders; added utils

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -16,6 +16,7 @@ import { ArrowDown, ArrowUp, CommentSvg, Important, LikeSvg, Pencil } from '@/sv
 import enableAuth from '../WithAuthAndTranslation';
 import { tokenApi } from '@/tokenApi';
 
+import { calculatePublishTime, mapEndings } from '@/utils';
 
 interface PostProps {
     post: PostType;
@@ -79,77 +80,6 @@ class Post extends Component<PostProps, PostState> {
             }),
         }));
         showNotification(this.props.t('deleteComment'), 'success', 2000);
-    }
-
-    calculatePublishTime = (): { num: number, timeType: string } => {
-        const now = new Date();
-        const published = new Date(this.props.post.creationDate);;
-        const diffMs = now.getTime() - published.getTime();
-
-        const seconds = Math.floor(diffMs / 1000);
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
-
-        const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
-
-        if (seconds < 60) return { num: seconds, timeType: "second" };
-        if (minutes < 60) return { num: minutes, timeType: "minute" };
-        if (hours < 24) return { num: hours, timeType: "hour" };
-        if (days < 7) return { num: days, timeType: "day" };
-        if (days < 30) return { num: Math.floor(days / 7), timeType: "week" };
-        if (days < 365) return { num: Math.floor(days / 30), timeType: "month" };
-
-        return { num: Math.floor(days / 365), timeType: 'year' };
-    };
-
-
-    mapEndings() {
-        let result = { likesEnding: "s", commentEnding: "s" };
-        const likesCount = this.state.likesCount;
-        const commentsCount = this.state.comments?.length || 0;
-
-        const lastLikes = likesCount % 10;
-        const lastTwoLikes = likesCount % 100;
-        const lastComments = commentsCount % 10;
-        const lastTwoComments = commentsCount % 100;
-
-        if (lastTwoLikes >= 11 && lastTwoLikes <= 14) {
-            result.likesEnding = "p2";
-        } else {
-            switch (lastLikes) {
-                case 1:
-                    result.likesEnding = "s";
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    result.likesEnding = "p1";
-                    break;
-                default:
-                    result.likesEnding = "p2";
-            }
-        }
-
-        if (lastTwoComments >= 11 && lastTwoComments <= 14) {
-            result.commentEnding = "p2";
-        } else {
-            switch (lastComments) {
-                case 1:
-                    result.commentEnding = "s";
-                    break;
-
-                case 2:
-                case 3:
-                case 4:
-                    result.commentEnding = "p1";
-                    break;
-                default:
-                    result.commentEnding = "p2";
-            }
-        }
-
-        return result;
     }
 
     toggleShowComments = () => {
@@ -279,7 +209,7 @@ class Post extends Component<PostProps, PostState> {
         const { profileImage, firstName, secondName } = this.state.author;
         const theme = useTheme.getState().theme;
 
-        const endings = this.mapEndings();
+        const endings = mapEndings(this.state.likesCount, this.state.comments?.length || 0);
         let likeEnding = "";
         let commentEnding = "";
 
@@ -304,7 +234,7 @@ class Post extends Component<PostProps, PostState> {
                 commentEnding = "commentsPlural2";
         }
 
-        const { num, timeType } = this.calculatePublishTime();
+        const { num, timeType } = calculatePublishTime(this.props.post.creationDate);
 
         return (
             <PostContainer theme={theme}>
