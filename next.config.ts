f2e1ec7dd1b4ff47f14305ca7f2b37e36b40618e
Diana Loyuk <wyrrrth.sip@gmail.com>
2025-12-01 23:28:38 +0300

    fix: fixed next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: 'https://willow-sip.github.io/Module10/',
+  basePath: '/Module10',
   assetPrefix: '/Module10/',
 
   typescript: {
