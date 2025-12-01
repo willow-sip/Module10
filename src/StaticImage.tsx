19eea053150845c4120876a6c533b1b07ccf6231
Diana Loyuk <wyrrrth.sip@gmail.com>
2025-12-01 20:56:50 +0300

    fix: fixed images

--- src/StaticImage.tsx
+++ src/StaticImage.tsx
@@ -1,11 +1,15 @@
 interface Props {
-    src: string;
-    alt: string;
-    width: number;
-    height: number;
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
 }
 
+const BASE_PATH = '/Module10';
+
 export default function StaticImage({ src, alt, width, height, ...props }: Props) {
-  const fixedSrc = src.startsWith('/') ? `/Module10${src}` : src;
+  const normalizedSrc = src.startsWith('/') ? src.slice(1) : src;
+  const fixedSrc = `${BASE_PATH}/${normalizedSrc}`;
+
   return <img src={fixedSrc} alt={alt} width={width} height={height} {...props} />;
 }
