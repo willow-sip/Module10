b655ac43b80ccfdc282232f41209db37956a69c6
Diana Loyuk <wyrrrth.sip@gmail.com>
2025-12-03 09:30:30 +0300

    fix: fixed static.yml

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.SW_GEN ? '' : '/Module10',
+  basePath: '/Module10',
   assetPrefix: '/Module10/',
 
   typescript: {
