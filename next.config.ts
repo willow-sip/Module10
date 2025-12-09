d3c908b18504ed08d850417af26fec690325d7bd
Diana Loyuk <wyrrrth.sip@gmail.com>
2025-12-09 15:33:27 +0300

    fix: static and config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: '/Module10',
+  basePath: '/Module10',
   // assetPrefix: '/Module10/',
 
   typescript: {
