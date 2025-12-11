90cd4e19e1c62c51cbd1877944dbe7c0ef2c5b21
willow-sip <wyrrrth.sip@gmail.com>
2025-12-11 10:58:46 +0000

    deploy: 3b9d2d021a61befda90b676ac26600ad2d9a35b4

---
+++ _next/static/sm7EAIcdJuj5ikXiA4mjq/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/Module10/Module10/mockServiceWorker.js",
+        "destination": "/Module10/mockServiceWorker.js"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
